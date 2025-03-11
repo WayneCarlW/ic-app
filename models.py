e41e1fde4a4951f2986560a0e935dc1c1243157a
WayneCarlW <waynecarlgeno@gmail.com>
2025-03-11 15:45:17 +0300

    user roles introduced!

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
 from flask_login import UserMixin
-from bson import ObjectId
-
 class User(UserMixin):
-    def __init__(self, user_id, email, password):
+    def __init__(self, user_id, email, role):
         self.id = user_id
         self.email = email
+        self.role = role
+class Product:
+    def __init__(self, product_id, name, price, description, image):
+        self.id = product_id
+        self.name = name
+        self.price = price
+        self.description = description
+        self.image = image
