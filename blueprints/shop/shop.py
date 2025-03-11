b51d279041b3599598118642a6a2aeac9311aad2
WayneCarlW <waynecarlgeno@gmail.com>
2025-03-11 22:30:51 +0300

    role based authentication and access

--- blueprints/shop/shop.py
+++ blueprints/shop/shop.py
@@ -1,6 +1,23 @@
 from flask import Blueprint
+from flask_login import current_user
+from flask import render_template
 
-shop = Blueprint('shop', __name__)
 
+shop = Blueprint('shop', __name__, template_folder='templates', static_folder='static')
 
+@shop.route('/')
+def home():
+    return render_template('shop/shelf.html', user=current_user)
+
+@shop.route('/cart')
+def cart():
+    return render_template('shop/cart.html', user=current_user)
+
+@shop.route('/checkout')
+def checkout():
+    return render_template('shop/checkout.html', user=current_user)
+
+@shop.route('/review')
+def review():
+    return render_template('shop/review.html', user=current_user)
 
--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 from flask_login import UserMixin
 class User(UserMixin):
-    def __init__(self, user_id, email, password):
+    def __init__(self, user_id, email, password, role):
         self.id = user_id
         self.email = email
         password = password
+        role = role
+
+    def is_admin(self):
+        return self.role == 'admin'    
 class Product:
     def __init__(self, product_id, name, price, description, image):
         self.id = product_id
