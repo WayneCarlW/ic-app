f61e5290defb5b29ea40b532519dccea289895d3
WayneCarlW <waynecarlgeno@gmail.com>
2025-03-17 15:47:33 +0300

    abstraction for the better and enhanced views

--- blueprints/admin/admin.py
+++ blueprints/admin/admin.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
+from blueprints.auth.auth import get_mongo_db
+from bson import ObjectId
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
@@ -10,3 +12,20 @@ def dashboard():
         flash('You are not authorized to access this page.', 'danger')
         return redirect(url_for('auth.login'))
     return render_template('admin/admin.html', user=current_user)
+
+@admin.route('/approve_user/<user_id>', methods=['PUT'])
+def approve_user(user_id):
+    db = get_mongo_db()
+    if db is None:
+        return redirect(url_for('admin.dashboard'))
+    db.users.update_one({'_id': ObjectId(user_id)}, {'$set': {'approved': True}})
+    flash('User approved successfully!', 'success')
+    return redirect(url_for('admin.dashboard'))
+
+@admin.route('/manage_manufacturers')
+def manage_manufacturers():
+    db = get_mongo_db()
+    if db is None:
+        return redirect(url_for('admin.dashboard'))
+    manufacturers = db.users.find({'role': 'manufacturer'})
+    return render_template('admin/manage_manufacturers.html', user=current_user, manufacturers=manufacturers)
--- models.py
+++ models.py
@@ -9,6 +9,9 @@ class User(UserMixin):
 
     def is_admin(self):
         return self.role == 'admin'
+    
+    def is_farmer(self):
+        return self.role == 'farmer'
 
     def is_manufacturer(self):
         return self.role == 'manufacturer'   
@@ -19,3 +22,17 @@ class Product:
         self.price = price
         self.description = description
         self.image = image
+class Farmer(User):
+    def __init__(self, farmer_id, name, location, supporting_docs):
+        self.id = farmer_id
+        self.name = name
+        self.location = location
+        self.supporting_docs = supporting_docs
+
+class Manufacturer(User):
+    def __init__(self, manufacturer_id, kam_id, name, industry_niche, supporting_docs):
+        self.id = manufacturer_id
+        self.kam_id = kam_id
+        self.name = name
+        self.industry_niche = industry_niche
+        self.supporting_docs = supporting_docs
