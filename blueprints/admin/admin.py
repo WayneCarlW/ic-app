1204b6a9fe393e1671f4df0c5fe553751aa6e27b
WayneCarlW <waynecarlgeno@gmail.com>
2025-03-18 18:02:29 +0300

    manufacturer approval working

--- blueprints/admin/admin.py
+++ blueprints/admin/admin.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_required
 from blueprints.auth.auth import get_mongo_db
 from bson import ObjectId
+from extensions import mongo, mail
+from flask_mail import Message
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
@@ -13,16 +15,48 @@ def dashboard():
         return redirect(url_for('auth.login'))
     return render_template('admin/admin.html', user=current_user)
 
-@admin.route('/approve_user/<user_id>', methods=['PUT'])
+@admin.route('/approve_user/<user_id>', methods=['GET','POST'])
+@login_required
 def approve_user(user_id):
-    db = get_mongo_db()
-    if db is None:
+    if not current_user.is_admin:
+        flash('You are not authorized to perform this action.', 'danger')
         return redirect(url_for('admin.dashboard'))
-    db.users.update_one({'_id': ObjectId(user_id)}, {'$set': {'approved': True}})
-    flash('User approved successfully!', 'success')
-    return redirect(url_for('admin.dashboard'))
+
+    db = get_mongo_db()
+    user = db.users.find_one({"_id": ObjectId(user_id)})
+
+    if not user:
+        flash("User not found", 'danger')
+        return redirect(url_for('admin.manage_manufacturers'))
+    
+    if user.get("approved"):
+        flash("User is already approved", 'info')
+        return redirect(url_for('admin.manage_manufacturers'))
+
+    # Approve user
+    db.users.update_one({"_id": ObjectId(user_id)}, {"$set": {"approved": True}})
+
+    # Send approval email
+    msg = Message(
+        "Your Account Has Been Approved!",
+        sender="cwasonga@kabarak.ac.ke",
+        recipients=[user["email"]]
+    )
+    msg.body = f"""
+    Hello {user["name"]},
+
+    Your account has been approved! You can now log in to your account.
+
+    Best regards,
+    Imitation Control Team
+    """
+    mail.send(msg)
+
+    flash("User approved and email sent successfully.", 'success')
+    return redirect(url_for('admin.manage_manufacturers'))
 
 @admin.route('/manage_manufacturers')
+@login_required
 def manage_manufacturers():
     db = get_mongo_db()
     if db is None:
